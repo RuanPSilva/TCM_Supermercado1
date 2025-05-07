e6f658cdf2a984b4f9de5fe49e9c235e40a35f88
RuanPSilva <rpfernandes71@gmail.com>
2025-05-06 22:51:19 -0300

    Estilização da pagina home

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TCM_Supermercado1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
